601dcf40165051ea3ed947946bc468b65e1a4cd5
Jimmy Huang <jimmy@netivism.com.tw>
2021-03-03 18:02:36 +0800

    refs #29985, fixes civicrm menu permission

--- src/Routing/Routes.php
+++ src/Routing/Routes.php
@@ -18,18 +18,38 @@ class Routes {
     // CiviCRM doesn't list optional path components. So we include 5 optional components for each route,
     // and let each default to empty string.
     foreach ($items as $path => $item) {
+      $requirement = [];
+
+      if (!empty($item['access_arguments'])) {
+        $perm = '';
+        $operator = '+';
+        if (!empty($item['access_arguments'][1])) {
+          $operator = $item['access_arguments'][1] == 'and' ? ',' : '+';
+        }
+        if (is_array($item['access_arguments'][0])) {
+          $perm = implode($operator, $item['access_arguments'][0]);
+        }
+        else {
+          $perm = $item['access_arguments'][0];
+        }
+        $requirement['_permission'] = $perm;
+      }
+      elseif ($item['access_callback'] == 1) {
+        $requirement['_access'] = 'TRUE';
+      }
+      elseif ($item['is_public']) {
+        $requirement['_access'] = 'TRUE';
+      }
+
       $route = new Route(
         '/' . $path . '/{extra}',
-        array(
+        [
           '_title' => isset($item['title']) ? $item['title'] : 'CiviCRM',
           '_controller' => 'Drupal\civicrm\Controller\CivicrmController::main',
           'args' => explode('/', $path),
           'extra' => '',
-        ),
-        array(
-          '_access' => 'TRUE',
-          'extra' => '.+',
-        )
+        ],
+        $requirement
       );
       $route_name = CivicrmHelper::parseURL($path)['route_name'];
       $collection->add($route_name, $route);
